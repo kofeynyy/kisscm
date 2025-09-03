54ca26cdb4f53c84cf9fd0fe15de1c0d57538a0b
kofeynyy <shipiloff.d@gmail.com>
2025-09-03 17:32:32 +0300

    Added new commands, catching exceptions

--- emu.py
+++ emu.py
@@ -1,7 +1,17 @@
+import shlex
+
 vfs_name = "docker$ "
 exit_cmd = 'exit'
-
 while True:
     command = input(vfs_name)
-    if command == exit_cmd: break
-    print(command)
+    parts = shlex.split(command)
+    if parts == exit_cmd:
+        break
+    if not parts:
+        continue
+    if parts[0] == 'ls':
+        print(parts)
+    elif parts[0] == 'cd':
+        print(parts)
+    else:
+        print(f'{command}: command not found')
